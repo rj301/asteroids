309f413c98f41270437f280e97eab1ee8a0fc7de
Reese Johnson <rj3zero1@gmail.com>
2025-12-15 23:06:43 -0700

    Start pygame launch game window, and quit game loop on user close window

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # Launch PyGame
+    pygame.init()
+
+    # Get a new instance of a GUI window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Game loop
+    while True:
+        log_state()
+        # Check if user has closed the game window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
