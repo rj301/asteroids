3843b782d7435b5427ace009a4dc9bf38d1e7c33
rj301 <rj3zero1@gmail.com>
2025-12-16 08:19:31 -0700

    Add new player constant, add player rotation and position update functionality

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ def main():
                 return
 
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
 
--- player.py
+++ player.py
@@ -4,7 +4,7 @@ Define Player class inheriting form CircleShape class
 
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
 
 
 class Player(CircleShape):
@@ -37,3 +37,24 @@ class Player(CircleShape):
             self.triangle(),
             LINE_WIDTH
         )
+
+    def rotate(self, dt):
+        """
+        Update the direction the player is facing
+        :param dt: delta time (amount of time since last frame drawn)
+        :return: None
+        """
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        """
+        Update player position when key pressed, starter code from Boot.dev
+        :param dt: delta time (amount of time since last frame drawn)
+        :return: None
+        """
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-1 * dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
